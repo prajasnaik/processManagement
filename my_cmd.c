425bd8e2d7f0bc16e6288ad4de7e32e73eecfbe9
prajasnaik <prajadnaik@gmail.com>
2024-03-18 00:19:10 +0530

    Added background execution support

--- my_cmd.c
+++ my_cmd.c
@@ -1,9 +1,10 @@
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <errno.h>
-#include <string.h>
-
+#include    <unistd.h>
+#include    <sys/types.h>
+#include    <sys/wait.h>
+#include    <errno.h>
+#include    <string.h>
+#include    <stdlib.h>
+#include    <stdio.h>
 
 #define     E_GENERAL   -1
 #define     E_OK        0
@@ -11,32 +12,49 @@
 #define     DISABLE     0
 
 
-int ec = E_OK;
+int         ec          =   E_OK;
 
-int fPathName = DISABLE;
-int fOptions  = DISABLE;
-int fBackground = ENABLE;
+int         fOptions    =   DISABLE;
+int         fBackground =   ENABLE;
 
-char *executablePath;
-char *optionsBuffer;
+char *      executablePath;
+char *      optionsBuffer;
 
-int     Help                    ();
-int     ProcessCommandLine      (char **, int);
-int     PerformOperations       ();
-
+int         Help                    ();
+int         ProcessCommandLine      (char **, int);
+int         PerformOperations       ();
+int         ExecuteInForeground     (char *);
+int         ExecuteInBackground     (char *);
+char **     VectorizeString         (char *, char *);
 
 // Main function
 int 
 main(int argc, char *argv[])
 {
+    for (int i = 0; i < argc; i ++)
+    {
+        int ec = write(0, argv[i], strlen(argv[i]));
+        if (ec == -1)
+            goto PRINT_ERROR;
+    }
     if (argc == 1)
             ec = Help();
     else
-    {
+    {        
         ec = ProcessCommandLine(argv, argc);
+        if (ec != E_OK)
+            goto PRINT_ERROR;
         ec = PerformOperations();
     }
+    
     return ec;
+    PRINT_ERROR:
+        char buffer[1024];
+        snprintf(buffer, 1024, "Error Code of C program: %d", errno);
+        ec = write(0, buffer, strlen(buffer));
+        if (ec == -1)
+            return errno;
+        return E_OK;
 }
 
 
@@ -57,15 +75,18 @@ ProcessCommandLine(char *commandLineArguments[], int argCount)
         switch (commandLineArguments[argno][1])
         {
         case 'e':
-            fPathName = ENABLE;
             if(argno + 1 == argCount)
             {
-                return E_GENERAL;
+                return 3;
             }
             executablePath = commandLineArguments[argno + 1];
             argno += 2;
             break;
         case 'o':
+            if(argno + 1 == argCount)
+            {
+                return E_GENERAL;
+            }
             fOptions = ENABLE;
             optionsBuffer = commandLineArguments[argno + 1];
             argno += 2;
@@ -88,18 +109,112 @@ ProcessCommandLine(char *commandLineArguments[], int argCount)
 int
 PerformOperations()
 {
-    
+    if(fBackground)
+    {
+        if (fOptions)
+        {
+            ec = ExecuteInBackground(optionsBuffer);
+        }
+        else
+        {
+            ec = ExecuteInBackground("none");
+        }
+    }
+    else
+    {
+        if(fOptions)
+            ec = ExecuteInForeground(optionsBuffer);
+        else
+        {
+            ec = ExecuteInForeground("none");
+        }
+    }
+    return ec;
 }
 
 
 
-char ** 
-VectorizeString(char *string, char *path)
+int
+ExecuteInForeground(char *options)
 {
-    char *testString = (char *) calloc(strlen(string), 1);
-    for (int i = 0; i < strlen(string); i ++)
+    int wstatus, retVal;
+    char **arguments = VectorizeString(options, executablePath);
+    int pid = fork();
+    if (pid == -1)
+        return errno;
+    else if (pid == 0)
     {
-        testString[i] = string[i];
+        int ec = execv(executablePath, arguments);
+        return errno;
+    }
+    else
+    {
+        int w = waitpid(pid, &wstatus, WUNTRACED);
+        if (w == -1)
+            return errno;
+        if (&wstatus != NULL)
+        {
+            if (WIFEXITED(wstatus))
+            {
+                retVal = WEXITSTATUS(wstatus);
+            }
+        }
+        
+    }
+
+    char buffer[1024];
+    snprintf(buffer, 1024, "%s - %d", "Process ID of Child", pid);
+    ec = write(0, buffer, strlen(buffer));
+    if (ec == -1)
+        return errno;
+    if (retVal != 0)
+    {
+        snprintf(buffer, 1024, "%s - %d", "Exit Code of Child: ", retVal);
+        ec = write(0, buffer, strlen(buffer));
+        if (ec == -1)
+            return errno;
+        ec = E_OK;
+    }
+    return ec;
+}
+
+int
+ExecuteInBackground(char * options)
+{
+    char **arguments = VectorizeString(options, executablePath);
+    int pid = fork();
+    if (pid == -1)
+        return errno;
+    else if (pid == 0)
+    {
+        int ec = execv(executablePath, arguments);
+        return errno;
+    }
+    else
+    {
+        char buffer[1024];
+        snprintf(buffer, 1024, "%s - %d", "Process ID of Child", pid);
+        ec = write(0, buffer, strlen(buffer));
+        if (ec == -1)
+            return errno;
+        return E_OK;
+    }
+}
+
+char ** 
+VectorizeString(char *optionsString, char *path)
+{
+    if (optionsString == "none")
+    {
+        char **options = (char **) calloc(2, sizeof(char *));
+        options[0] = path;
+        options[1] = NULL;
+        return options;
+    }
+    char *testString = (char *) calloc(strlen(optionsString), 1);
+    for (int i = 0; i < strlen(optionsString); i ++)
+    {
+        testString[i] = optionsString[i];
     }
     int i = 0;
     int elementCount = 1;
@@ -114,11 +229,15 @@ VectorizeString(char *string, char *path)
     options[0] = path;
     char * token = strtok(testString, " ");
     i = 1;
-   // loop through the string to extract all other tokens
     while( token != NULL ) {
         options[i++] = token;
         token = strtok(NULL, " ");
     }
     options[i] = NULL;
     return options;
+}
+
+int Help()
+{
+    return 0;
 }
