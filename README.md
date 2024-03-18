8624b03e3f7afbefc11c3926ce7bfcc9da9a6b00
prajasnaik <prajadnaik@gmail.com>
2024-03-18 23:06:29 +0530

    fixed bugs and added help functionality and readme

--- README.md
+++ README.md
@@ -1,9 +1,26 @@
 # Process Execution
 
-#### Contributors: Prajas Naik
+## How to use the program
 
-In this program, I have chosen to make the default behavior as the process running in the foreground, but this can be changed optionally.
+#### Compiling and Execution:
 
-If the number of arguments passed to the process is insufficient, it will terminate with error code -1. This was also a design decision to allow the process to indicate that the issue was with the arguments and not any other specific error.
+```Bash
+        make
+        ./my_cmd -e <ExecutablePath> -o <Options> [-b | -f]
+``` 
+Here, the -b flag is used to indicate that the child process will run in the background, and the -f flag indicates that it will run in the foreground. The default behavior is foreground execution. 
 
-Another design decision I made was to report only one error of write system call in standard output when printing which error code our C program is reporting. If the write system call fails another time, the value of errno is returned and no error code is written to standard output. 
+## Design Decisions
+
+In this program, we have chosen to make the default behavior as the process running in the foreground, but this can be changed optionally. This behavior was chosen to ensure that the process executes either in foreground or background but not both.
+
+If the number of arguments passed to the process is insufficient, it will terminate with error code -1. This was also a design decision to allow the process to indicate that the issue was with the arguments and not any other specific error. Similarly, any other error related to arguments will return -1
+
+Another design decision we made was to report only one error of write system call in standard output when printing which error code our C program is reporting. If the write system call fails another time, the value of errno is returned and no error code is written to standard output. 
+
+In the program, we have chosen to print exit code of child only if the value is not 0, otherwise no error code is printed. This is to remove redundancy.
+Similarly, no error code is printed for the C program if no error has arisen during the process execution. If any error occurs, it will be detected and printed accordingly.
+
+In the program if no arguments are given, the help message will be displayed with information of how to use the program. If options are given but no executable path is specified, the program will print message specifying that executable path must be specified. This decision was made to meet assignment requirements while retaining the default help functionality.
+
+Finally, the program only prints the exit code of the child. It does not check whether the core was dumped in the child or whether it incountered any signals. This behavior was chosen as the assignment only specifed that the exit code has to be printed, and no mention was made of the signals received by the child. 
--- my_cmd.c
+++ my_cmd.c
@@ -1,3 +1,7 @@
+// @date: 18 March 2024
+// Simple process management program
+
+// Include Statements
 #include    <unistd.h>
 #include    <sys/types.h>
 #include    <sys/wait.h>
@@ -6,21 +10,26 @@
 #include    <stdlib.h>
 #include    <stdio.h>
 
+//Define Statements (Macros)
 #define     E_GENERAL   -1
 #define     E_OK        0
 #define     ENABLE      1
 #define     DISABLE     0
 #define     MAX_SIZE    1024
 
-
+// Global Variable for Error Code
 int         ec          =   E_OK;
 
-int         fOptions    =   DISABLE;
-int         fBackground =   DISABLE;
+// Global Variable Flags for deciding which operation to perform
+int         fOptions            =   DISABLE;
+int         fBackground         =   DISABLE;
+int         fExecutablePath     =   DISABLE;
 
+// Buffer pointers for storing paths for each function
 char *      executablePath;
 char *      optionsBuffer;
 
+// Function Declarations
 int         Help                    ();
 int         ProcessCommandLine      (char **, int);
 int         PerformOperations       ();
@@ -34,23 +43,30 @@ int         PrintError              (int );
 int 
 main(int argc, char *argv[])
 {
-    for (int i = 0; i < argc; i ++)
+    for (int i = 0; i < argc; i ++) //used to print all the command line arguments received by the process separated by a space
     {
-        ec = write(0, argv[i], strlen(argv[i]));
-        if (ec == -1)
+        ec = write(STDOUT_FILENO, argv[i], strlen(argv[i]));
+        if (ec == E_GENERAL)
         {
             ec = PrintError(errno);
             return ec;
         }   
-        ec = write(0, " ", 1);
-        if (ec == -1)
+        ec = write(STDOUT_FILENO, " ", 1);
+        if (ec == E_GENERAL)
         {
             ec = PrintError(errno);
             return ec;
         }
     }
     if (argc == 1)
-            ec = Help();
+    {
+        ec = Help(); //Help is called if no options are given
+        if (ec != E_OK)
+        {
+            ec = PrintError(ec);
+            return ec;
+        }
+    }
     else
     {        
         ec = ProcessCommandLine(argv, argc);
@@ -91,6 +107,7 @@ ProcessCommandLine(char *commandLineArguments[], int argCount)
             {
                 return E_GENERAL;
             }
+            fExecutablePath = ENABLE;
             executablePath = commandLineArguments[argno + 1];
             argno += 2;
             break;
@@ -109,15 +126,33 @@ ProcessCommandLine(char *commandLineArguments[], int argCount)
             break;
         case 'f':
             fBackground = DISABLE;
+            argno += 1;
             break;
         default:
-            return E_OK;
+            return E_GENERAL;
             break;
         }
     } 
+    if(!fExecutablePath)
+    {
+        char *buf = "\nNo executable path specified, please use -e flag to provide path.";
+        ec = write(STDOUT_FILENO, buf, strlen(buf));
+        if (ec == E_GENERAL)
+        {
+            ec = PrintError(errno);
+            return ec;
+        }
+        return E_GENERAL;
+    }
     return E_OK; 
 }
 
+
+//  function: PerformOperations
+//      This function calls and executes appropriate functions based on
+//      which flags were set after processing command line.
+//  @param: None
+//  @return: Integer Error Code
 int
 PerformOperations()
 {
@@ -145,30 +180,35 @@ PerformOperations()
 }
 
 
-
+//  function: ExecuteInForeground
+//      This function executes a given binary specified by a path in the foreground
+//      The options for this binary are optional and must be given as "none" if no
+//      options are to be applied.
+//  @param: pointer to a character array containing options string
+//  @return: integer error code
 int
 ExecuteInForeground(char *options)
 {
     int wstatus, retVal;
-    char **arguments = VectorizeString(options, executablePath);
+    char **arguments = VectorizeString(options, executablePath); 
     int pid = fork();
-    if (pid == -1)
+    if (pid == E_GENERAL)
         return errno;
     else if (pid == 0)
     {
         int ec = execv(executablePath, arguments);
-        return errno;
+        return errno; //only returns if error occurred in execv() system call
     }
     else
     {
         int w = waitpid(pid, &wstatus, WUNTRACED);
-        if (w == -1)
+        if (w == E_GENERAL)
             return errno;
         if (&wstatus != NULL)
         {
             if (WIFEXITED(wstatus))
             {
-                retVal = WEXITSTATUS(wstatus);
+                retVal = WEXITSTATUS(wstatus); //finds exit status of child if not 0
             }
         }
         
@@ -176,10 +216,10 @@ ExecuteInForeground(char *options)
 
     char buffer[MAX_SIZE];
     snprintf(buffer, MAX_SIZE, "\n%s - %d", "Process ID of Child", pid);
-    ec = write(0, buffer, strlen(buffer));
-    if (ec == -1)
+    ec = write(STDOUT_FILENO, buffer, strlen(buffer));
+    if (ec == E_GENERAL)
         return errno;
-    if (retVal != 0)
+    if (retVal != E_OK)
     {
         snprintf(buffer, MAX_SIZE, "\n%s - %d", "Exit Code of Child: ", retVal);
         ec = write(0, buffer, strlen(buffer));
@@ -190,77 +230,107 @@ ExecuteInForeground(char *options)
     return ec;
 }
 
+//  function: ExecuteInForeground
+//      This function executes a given binary specified by a path in the background
+//      The options for this binary are optional and must be given as "none" if no
+//      options are to be applied.
+//  @param: pointer to a character array containing options string
+//  @return: integer error code
 int
 ExecuteInBackground(char * options)
 {
     char **arguments = VectorizeString(options, executablePath);
     int pid = fork();
-    if (pid == -1)
-        return errno;
+    if (pid == E_GENERAL)
+        return errno; 
     else if (pid == 0)
     {
         int ec = execv(executablePath, arguments);
-        return errno;
+        return errno; //only returns if error occurred in execv() system call
     }
     else
     {
         char buffer[MAX_SIZE];
         snprintf(buffer, MAX_SIZE, "\n%s - %d", "Process ID of Child", pid);
         ec = write(0, buffer, strlen(buffer));
-        if (ec == -1)
+        if (ec == E_GENERAL)
             return errno;
         return E_OK;
     }
 }
 
+
+//  function: VectorizeString
+//      This function takes a string of character options as well as executable path and
+//      constructs a null terminated array of strings that can be passed to the execv() 
+//      system call. The format of the array is similar to the command line options received
+//      by processes as command line arguments. 
+//  @param: pointer to a character array containing options string
+//  @param: pointer to a character array containing an executable path
+//  @return: pointer to the vectorized array of strings
 char ** 
 VectorizeString(char *optionsString, char *path)
 {
     if (optionsString == "none")
     {
-        char **options = (char **) calloc(2, sizeof(char *));
+        char **options = (char **) calloc(2, sizeof(char *)); //gets a pointer to an array of pointers
         options[0] = path;
         options[1] = NULL;
         return options;
     }
-    char *testString = (char *) calloc(strlen(optionsString), 1);
+    char *testString = (char *) calloc(strlen(optionsString), 1); //gets a new character array
     for (int i = 0; i < strlen(optionsString); i ++)
     {
-        testString[i] = optionsString[i];
+        testString[i] = optionsString[i]; //sets the new array equal to the old one
     }
     int i = 0;
-    int elementCount = 1;
-    int length = strlen(testString + 1);
+    int elementCount = 1; //Set to one because an array without spaces still has one element
+    int length = strlen(testString + 1); // 
     for (i = 0; i < length; i ++)
     {
         if(testString[i] == ' ')
-            elementCount++;
+            elementCount++; //checks for number of spaces in the array
     }
 
-    char **options = (char **) calloc(elementCount + 2, sizeof(char *));
+    char **options = (char **) calloc(elementCount + 2, sizeof(char *)); // Is + 2 because of the addition of the path at the start and the null character at the end
     options[0] = path;
-    char * token = strtok(testString, " ");
+    char * token = strtok(testString, " "); //splits the string on spaces and appends to argument array
     i = 1;
     while( token != NULL ) {
-        options[i++] = token;
-        token = strtok(NULL, " ");
+        options[i++] = token; 
+        token = strtok(NULL, " "); 
     }
-    options[i] = NULL;
+    options[i] = NULL; //terminates array with null character
     return options;
 }
 
-int Help()
+// function: Help
+//      Provides a basic message on how to use the program.
+//  @param: None
+//  @return: Integer Error Code
+int 
+Help()
 {
-    return 0;
+    char *helpMessage = "\n\tUsage:\n\t./my_cmd -e <ExecutablePath> -o <OptionsString> [-f|-b] \n\tFor more info, please refer to README file\n";
+    int length = strlen(helpMessage);
+    int error = write(STDOUT_FILENO, helpMessage, length);
+    if (error == E_GENERAL)
+        return errno;
+    return E_OK;
 }
 
+//  function: PrintError
+//      This function takes an error code that occurred during a program's
+//      execution and prints a message onto standard output
+//  @param: integer error code that needs to be printed
+//  @return: integer error code 
 int
 PrintError(int errorCode)
 {
     char buffer[MAX_SIZE];
     snprintf(buffer, MAX_SIZE, "\nError Code of C program: %d", errorCode);
-    ec = write(0, buffer, strlen(buffer));
-    if (ec == -1)
+    ec = write(STDOUT_FILENO, buffer, strlen(buffer));
+    if (ec == E_GENERAL)
         return errno;
     return E_OK;
 }
